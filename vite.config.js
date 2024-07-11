d9786515ee354f334b08230a10668ddd23851060
Sourav Paitandy <souravpaitandy@gmail.com>
2024-07-11 18:02:48 +0530

    Modified-1: vite.config.js.

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/",
+  base: "/portfolio/", // Change this line
   build: {
     outDir: "dist",
   }
